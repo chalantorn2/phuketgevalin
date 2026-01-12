af794fa8f497e7704efbacd84ec2d793558591d9
chalantorn2 <chalantorn2@gmail.com>
2026-01-12 23:26:43 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "24px",
+        fontWeight: "bold",
+      }}
+    >
+      กำลังอยู่ในการพัฒนา
+    </div>
+  );
+}
